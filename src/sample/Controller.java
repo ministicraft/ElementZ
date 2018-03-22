630961188935a7e072e092f703878903177a95cf
Claveau Alexandre <a.claveau29@gmail.com>
2018-03-22 16:23:02 +0100

    correction on load function

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,13 +12,22 @@ public class Controller{
     public Button startBouton;
     public GridPane gridPaneBoule;
 
-    private ImageView[] imageBoules = new ImageView[7];
+    private ImageView[][] imageBoules = new ImageView[8][8];
     private ElementZ_Model EZJeu;
 
     private void loadImage (){
         //Je charge mes images
-        for (int i=0; i<7; i++){
-            imageBoules[i] = new ImageView("/boules_isen/boule_" + i + ".jpg");
+        int k = 0;
+        for (int i=0; i<8; i++){
+            for (int j=0; j<8; j++){
+                if (k<=6){
+                    imageBoules[i][j] = new ImageView("/boules_isen/boule_" + k + ".jpg");
+                    k++;
+                }else{
+                    k=0;
+                    imageBoules[i][j] = new ImageView("/boules_isen/boule_" + k + ".jpg");
+                }
+            }
         }
     }
 
@@ -32,12 +41,7 @@ public class Controller{
     }
 
     private void affectBoules(){
-        int k = 0;
-        for (int i=0; i<8; i++){
-            for (int j=0; j<8; j++){
+        gridPaneBoule = new GridPane();
 
-            }
-        }
     }
-
 }
