bed5fdbee4d9f19d6b8c257c731a1f25f9aaa584
Claveau Alexandre <a.claveau29@gmail.com>
2018-03-23 10:01:04 +0100

    New loading images

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,17 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class Controller{
 
@@ -14,15 +20,42 @@ public class Controller{
     public GridPane gridPaneBoule;
 
     private ElementZ_Model EZJeu;
+    private boolean selected = false;
+    private int selectedX;
+    private int selectedY;
+    private int selectedXP;
+    private int selectedYP;
     private Image[] imageBoules = new Image[8];
+    private Image[] imageBoulesHover = new Image[8];
+    private Image[] imageBoulesSelected = new Image[8];
 
-    private void loadImage(){
-        for (int i=0; i<7; i++){
+    //--------------------------------------------------------------------------
+    // Je viens dans cette méthode, charger mes images afin de puvoir les
+    // utiliser plus tard.
+    //--------------------------------------------------------------------------
+    private void loadImageSimple(){
+        for (int i=1; i<7; i++){
             imageBoules[i]= new Image("/boules_isen/boule_"+ i +".jpg");
         }
     }
 
-    private void initComponents (){
+    private void loadImageHover(){
+        for (int i=1; i<7; i++){
+            imageBoulesHover[i]= new Image("/boules_isen/boule_o_"+ i +".jpg");
+        }
+    }
+
+    private void loadImageSelected(){
+        for (int i=1; i<7; i++){
+            imageBoulesSelected[i]= new Image("/boules_isen/boule_s_"+ i +".jpg");
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Cette méthode me permet quand à elle de venir affecter les boules en
+    // fontion de la matrice de jeu.
+    //--------------------------------------------------------------------------
+    private void affectBalls (){
         for (int i=0; i<8; i++){
             for (int j=0; j<8; j++){
                 gridPaneBoule.add(new ImageView(imageBoules[EZJeu.getXY(i,j)]),i,j);
@@ -30,12 +63,57 @@ public class Controller{
         }
     }
 
+    //--------------------------------------------------------------------------
+    // Ici je viens avec cette méthode lancer le jeu
+    //--------------------------------------------------------------------------
     @FXML
     private void jButtonStart() {
         System.out.println("OK Start");
-        loadImage();
+        loadImageSimple();
+        loadImageHover();
+        loadImageSelected();
         EZJeu = new ElementZ_Model();
-        initComponents();
+        affectBalls();
         scoreGame.setText("0");
     }
+
+    //private void dragAndDrop (){
+    //    gridPaneBoule.setOnDragDetected(new EventHandler<DragEvent>() {
+
+    //    }
+    //}
+
+    /*private void clic_sur_boule(MouseEvent event){
+        setBorder(event);
+        Point p = getCoords(event);
+        System.out.println(p.getX()+" , "+p.getY());
+    }
+
+    public void setBorder(MouseEvent event){
+        Point coords = getCoords(event);
+        if (!selected){
+            event.getComponent().setBackground(Color.white);
+            selectedX = (int) (coords.getX())-1;
+            selectedY = (int) (coords.getY())-1;
+        }else {
+            event.getComponent().setBackground(Color.black);
+            selectedXP = (int) (coords.getX())-1;
+            selectedYP = (int) (coords.getY())-1;
+            EZJeu.play(selectedX,selectedY,selectedXP,selectedYP);
+            affectBalls();
+            scoreGame.setText(""+EZJeu.getScore());
+        }
+    }
+
+    private Point getCoords (MouseEvent event){
+        int k = 1;
+        for (int i=0; i<8; i++){
+            for (int j=0; j<8; j++){
+                if (event.getComponent().getX() == k++ && event.getComponent().getY()== k++){
+                    return new Point(i+1,j+1);
+                }
+            }
+        }
+        return null;
+    }*/
 }
