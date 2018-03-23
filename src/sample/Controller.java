7b9b02530d46f6b14ef58d7763cc15c955b04d3e
ministicraft <worldtocraft@gmail.com>
2018-03-23 19:17:37 +0100

    code clean + change name

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,11 +8,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import sun.rmi.runtime.Log;
 
 import java.net.URI;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Controller{
