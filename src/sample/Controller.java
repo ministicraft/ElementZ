a4935859692630be2d9faa5a99f034218e9798bf
Claveau Alexandre <a.claveau29@gmail.com>
2018-03-23 16:49:29 +0100

    delete comments

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -95,43 +95,4 @@ public class Controller{
 
         System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
     }
-    //private void dragAndDrop (){
-    //    gridPaneBoule.setOnDragDetected(new EventHandler<DragEvent>() {
-
-    //    }
-    //}
-
-    /*private void clic_sur_boule(MouseEvent event){
-        setBorder(event);
-        Point p = getCoords(event);
-        System.out.println(p.getX()+" , "+p.getY());
-    }
-
-    public void setBorder(MouseEvent event){
-        Point coords = getCoords(event);
-        if (!selected){
-            event.getComponent().setBackground(Color.white);
-            selectedX = (int) (coords.getX())-1;
-            selectedY = (int) (coords.getY())-1;
-        }else {
-            event.getComponent().setBackground(Color.black);
-            selectedXP = (int) (coords.getX())-1;
-            selectedYP = (int) (coords.getY())-1;
-            EZJeu.play(selectedX,selectedY,selectedXP,selectedYP);
-            affectBalls();
-            scoreGame.setText(""+EZJeu.getScore());
-        }
-    }
-
-    private Point getCoords (MouseEvent event){
-        int k = 1;
-        for (int i=0; i<8; i++){
-            for (int j=0; j<8; j++){
-                if (event.getComponent().getX() == k++ && event.getComponent().getY()== k++){
-                    return new Point(i+1,j+1);
-                }
-            }
-        }
-        return null;
-    }*/
 }
